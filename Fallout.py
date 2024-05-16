4b0164f5ec046a809bb228f7d0d8d930f8d8db90
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-05-16 17:51:23 +0100

    fallout function running

--- Fallout.py
+++ Fallout.py
@@ -15,14 +15,24 @@ word = random.choice(randomWords).upper()
 print('Words:', '  '.join(randomWords).upper())
 
 def fallout():
-    print(word)
-    guess = input('Guess a word => ').upper()
-    count = 0
-    for letter in word:
-        if letter in guess:
-            count = count + 1
-    print(guess)
-    print(count)
+    lives = 5
+    guess = ''
+    while guess != word and lives > 0:
+        guess = input('Guess a word => ').upper()
+        count = 0
+        for letter in word:
+            if letter in guess:
+                count = count + 1
+        lives = lives - 1
+        if guess == word:
+            print(f'Congrats, the word was {word}')
+        elif lives == 0:
+            print(f'\nGAME OVER\nThe word was {word}')
+        else:
+            print(f'Your guess: {guess}\nNumber of matching letters: {count}\nLives left: {lives}\n')
+        
+        
+    
     
     
 fallout()
